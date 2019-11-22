2360051de82fee74fab9bdca9351097ea19cb563
Lucas Guedes Barboza <lucasguedesbarboza@gmail.com>
2019-11-22 16:54:34 -0300

    Modify params and .gitignore

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'http://apl.utfpr.edu.br/extensao/certificados/listaPublica'
-myobj = {'txtCampus' : '3', 'txtAno' : '', 'txtEvento' : '828'}
+#_params = {'txtCampus' : '3', 'txtAno' : '', 'txtEvento' : '828'}
+_params = {'txtCampus' : '3', 'txtAno' : '', 'txtEvento' : ''}
 
-html_doc = requests.post(url, data = myobj)
-
+html_doc = requests.post(url, data = _params)
 
 soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-
-a = soup.find('select', {'name' : 'txtEvento'})
+a = soup.find('select', {'name' : 'txtEvento'}).attrs
 
 print(a)
 
